fee283bce459181eee9ed7f2de69eda5759f9891
Guillermo <guillermoxr24@gmail.com>
2024-09-21 19:33:21 -0300

    A

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-500">
+    Hello world!
+  </h1>
+  )
+}
+
---
+++ src/pages/index.pages.js
@@ -0,0 +1,7 @@
+import Homepage from "./Homepage.jsx";
+
+
+export {
+    Homepage,
+   
+}
